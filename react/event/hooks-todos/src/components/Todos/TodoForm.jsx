4f37cf1fd02bc113e7a6146a21bd5cf9837cd334
alicia <2354602665@qq.com>
2025-07-08 12:58:03 +0800

    study

---
+++ react/event/hooks-todos/src/components/Todos/TodoForm.jsx
@@ -0,0 +1,17 @@
+import { 
+    useState // 私有状态
+} from 'react'
+const TodoForm = () =>{
+    // JSX 一定得有唯一的最外层元素 树状 树来编译解析JSX，
+    return (
+        <>
+            <h1 className="header">
+                TodoForm
+            </h1>
+            <form>
+
+            </form>
+        </>
+    )
+}
+export default TodoForm
